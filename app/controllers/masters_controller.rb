f625dc3d4e3b984bd41c423e9c0c59762616a878
aleksio <14111984Alexey>
2015-11-07 17:16:20 +0300

    master password read/set

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -4,10 +4,12 @@ class MastersController < ApplicationController
 
   before_filter :find_project, :authorize
 
+  # form for new master password for project vault
   def new
     @master_password = MasterPassword.new
   end
 
+  # create new master password for project vault
   def create
     passwords = params[:master_password]
     if passwords[:password] == passwords[:password_repeat]
@@ -20,22 +22,31 @@ class MastersController < ApplicationController
     end
   end
 
+  # form for enter master password for decrypt vault
   def decrypt
     @master_password = MasterPassword.new
   end
 
+  # check master password
   def decrypt_post
     password = master_params[:password]
-    Rails.cache.write(:master, password, expires_in: 1.minute)
     digest = Digest::MD5.hexdigest(password)
     master_digest = MasterPassword.find_by(:project_id => @project)
-    redirect_to project_vaults_path
+    if digest == master_digest.password
+      Rails.cache.write(:master, password, expires_in: 1.minute)
+      redirect_to project_vaults_path
+    else
+      flash[:error] = 'Wrong master password!'
+      redirect_to '/projects/' + @project.identifier + '/decrypt'
+    end
   end
 
+  # get params for creating new master password
   def master_params
     params.require(:master_password).permit(:password)
   end
 
+  # find project id
   def find_project
     @project = Project.find(params[:project_id])
   end
