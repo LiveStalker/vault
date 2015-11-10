815f10d05acd74ba6d2ddd2812b2711da9394cdc
aleksio <14111984Alexey>
2015-11-10 14:21:49 +0300

    first version

--- app/models/vault.rb
+++ app/models/vault.rb
@@ -2,5 +2,6 @@ class Vault < ActiveRecord::Base
   unloadable
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   belongs_to :project, :class_name => "Project", :foreign_key => "project_id"
+  attr_accessible :host, :login, :password, :notes
   validates_presence_of :host, :login, :password
 end
